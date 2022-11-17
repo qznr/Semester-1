a71fa340009585d368be55fff2d42c7171ccd89d
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2022-11-17 20:17:01 +0700

    upload

---
+++ CountOddAndEven.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CountOddAndEven {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int min = in.nextInt();
+        int max = in.nextInt();
+        int ganjil = (max-min)/2;
+        if (max%2!=0 || min%2!=0) ganjil++;
+        int genap = max-min+1-ganjil;
+        System.out.println(genap);
+    }
+}
