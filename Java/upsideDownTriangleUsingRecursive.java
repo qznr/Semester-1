6800ef9d2b67bcbc9b6ba03991c93503d41971ee
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2022-12-04 14:02:28 +0700

    relocate

---
+++ Java/upsideDownTriangleUsingRecursive.java
@@ -0,0 +1,24 @@
+package Java;
+import java.util.Scanner;
+
+public class upsideDownTriangleUsingRecursive {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        pangkat(a,a);
+        
+    }
+    public static int pangkat(int n, int m){
+        int count = m;
+        if (n>0){
+            n = n-1;
+            System.out.print("* ");
+            if (n==0){
+                n = --count;
+                System.out.println("");
+            }
+            return (pangkat(n,count));
+          }
+          return n;
+    }
+}
---
+++ Python/HelloWorld.py
@@ -0,0 +1,5 @@
+def main():
+    print('こんにちは世界！')
+
+if __name__ == '__main__':
+    main()
